c5ff60e58197e96eed067bd33a8ef11bcce0d2ba
ASHISH PRAJAPATI <aprajapatiji25@gmail.com>
2026-08-25 00:03:48 +0530

    Add files via upload

---
+++ week-06/Strings_02.py
@@ -0,0 +1,27 @@
+# Even Characters first and Odd Characters reversed
+# Given a string, return a string with the characters in the even indices first and the characters in the odd indices next but in reversed order.
+
+# Example For the input "abcde",
+
+# even chars = "ace"
+# odd chars = "bd"; odd chars reversed = "db"
+# result = "acedb"
+
+def even_first_odd_reversed(s: str) -> str:
+    '''Return a string with the characters in the even indices first 
+    and the characters in the odd indices reversed next.
+
+    Arguments:
+    s: str - the input string
+
+    Return:
+    str - modified string
+
+    Example:
+    >>> even_first_odd_reversed('abcde')
+    'acedb'
+    >>> even_first_odd_reversed('python')
+    'ptonhy'
+    '''
+    return s[::2] + s[1::2][::-1]
+    
